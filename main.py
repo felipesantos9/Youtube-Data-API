c13ca4989da791554505c99af0a4e413ff798620
Felipe Santos <felipessantos2004@gmail.com>
2024-03-05 15:51:14 -0300

    encontrando playlists das corridas

--- main.py
+++ main.py
@@ -8,4 +8,8 @@ channel_response = youtube.channels().list(part='id', forUsername='Formula1').ex
 
 channel_id = channel_response["items"][0]["id"]
 
-print(channel_id)
+channel_playlists = youtube.playlists().list(part=['snippet','contentDetails'],channelId=channel_id, maxResults=50 ).execute()
+
+for playlist in channel_playlists['items']:
+    if "Formula 1" in playlist['snippet']['title'] and "2023" in playlist['snippet']['title']:
+        print(playlist['snippet']['title'])
